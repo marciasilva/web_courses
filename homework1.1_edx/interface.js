7a5abd193c9f9fe255a1a375cfe91700ce5cf5c3
unknown <masilva@bhz01masilva.fitec.org.br>
2016-03-11 10:04:34 -0300

    git ignore for edx project, including the require packages

--- homework1.1_edx/interface.js
+++ homework1.1_edx/interface.js
@@ -3,6 +3,12 @@
  */
 exports.insert = function(db, doc, callback) {
   // TODO: implement
+  db.collection('movies').insert(doc, function(error, result){
+  	if(error){
+  		console.log('error insert doc: ', error);
+  		process.exit(1);
+  	}
+  });
   callback(null);
 };
 
@@ -13,6 +19,19 @@ exports.insert = function(db, doc, callback) {
  *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
  */
 exports.byDirector = function(db, director, callback) {
+	//console.log('received director from fuction: ', director);
+	var query = {'director' : director};
+	db.collection('movies').find(query).sort({'title':1}).toArray(function(error, docs){
+		if(error){
+			console.log('error query movies for director: ', error);
+			process.exit(1);
+		}
+		//console.log('length: ' docs.length);
+		console.log('Found docs: ');
+		docs.forEach(function(doc){
+			console.log(JSON.stringify(doc));
+		});
+		callback(null, docs);
+	});
   // TODO: implement
-  callback(null, []);
 };
